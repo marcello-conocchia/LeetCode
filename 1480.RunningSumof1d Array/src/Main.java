3317db5d720bb507a330352a65bc9ebfa2e89ce6
Marcello Conocchia <conocchia27@icloud.com>
2025-01-27 21:03:46 +0100

    Solved

---
+++ 1480.RunningSumof1d Array/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main {
+
+public static void main(String [] args) {
+	int[] nums = {1, 2, 3, 4, 5};
+	int[] result = runningSum(nums);
+	System.out.println(Arrays.toString(result)); 
+
+}
+	public static int[] runningSum(int[] nums) {
+	
+		int[] runningSum = new int [nums.length];
+		
+		runningSum[0] = nums[0];
+		
+		for(int i = 1; i < nums.length; i++ ) {
+			runningSum[i] = runningSum[i -1] + nums[i];
+			System.out.println(runningSum[i]);
+		}
+		
+		return runningSum;
+        
+    }}
